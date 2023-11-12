b1f440bd553ede9d6c8552759559e2a116a23df0
AVasilaki <poseydon463@gmail.com>
2023-11-12 08:45:45 +0200

    Create MovieDetailsPage.styled.js

---
+++ src/pages/MovieDetails/MovieDetailsPage.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+export const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 10px;
+  background-color: #fff;
+  border-radius: 4px;
+`;
+// img {
+//   display: block;
+//   max-width: 100%;
+// }
+
+// .card.flex-start-aligned {
+//   align-items: flex-start;
+// }
+
+// .card + .card {
+//   margin-top: 20px;
+// }
