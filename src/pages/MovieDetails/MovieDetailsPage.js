dce324a842142976be313c6001e43d704f5270c0
AVasilaki <poseydon463@gmail.com>
2023-11-12 08:45:26 +0200

    Update MovieDetailsPage.js

--- src/pages/MovieDetails/MovieDetailsPage.js
+++ src/pages/MovieDetails/MovieDetailsPage.js
@@ -1,17 +1,15 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import { fetchApi } from 'takeApi';
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import {
+  Container,
+  Header,
+  Link,
+} from 'components/SharedLayout/SharedLayout.styled';
 // import Casts from 'pages/Cast/CastPage';
-const StyledLink = styled(NavLink)`
-  color: black;
-
-  &.active {
-    color: orange;
-  }
-`;
-
+import { Wrapper } from './MovieDetailsPage.styled';
 export const MovieDetails = () => {
   const [movieDetail, setMovieDetail] = useState({});
   const [genres, setGenres] = useState([]);
@@ -39,24 +37,30 @@ export const MovieDetails = () => {
   return (
     <>
       <div>
-        <img
-          src={`https://image.tmdb.org/t/p/w300/${movieDetail.poster_path}`}
-          alt=""
-        ></img>
-        <h1>{movieDetail.title || movieDetail.name}</h1>
-        <h3>User score: {movieDetail.vote_average}</h3>
-        <h2>Overview</h2>
-        <p>{movieDetail.overview}</p>
-        <h3>Genres</h3>
-        <p>{genres.map(genr => genr.name).join(',')}</p>
+        <Wrapper>
+          <img
+            src={`https://image.tmdb.org/t/p/w300/${movieDetail.poster_path}`}
+            alt=""
+          ></img>
+          <div>
+            <h1>{movieDetail.title || movieDetail.name}</h1>
+            <h3>User score: {movieDetail.vote_average}</h3>
+            <h2>Overview</h2>
+            <p>{movieDetail.overview}</p>
+            <h3>Genres</h3>
+            <p>{genres.map(genr => genr.name).join(',')}</p>
+          </div>
+        </Wrapper>
       </div>
 
-      <div>
-        <nav>
-          <StyledLink to="cast">Cast</StyledLink>
-          <StyledLink to="reviews">Reviews</StyledLink>
-        </nav>
-      </div>
+      <Container>
+        <Header>
+          <nav>
+            <Link to="cast">Cast</Link>
+            <Link to="reviews">Reviews</Link>
+          </nav>
+        </Header>
+      </Container>
 
       <Outlet></Outlet>
     </>
